1534f313348f0bc0a9ea22b3c366b8da06344f89
dgoudie <34222208+dgoudie@users.noreply.github.com>
2020-01-29 23:30:19 -0600

    removed fetch, added chrome.storage

--- background.js
+++ background.js
@@ -1,21 +1,32 @@
-const GO_REGEX = /http:\/\/go\/(.+)\//i;
-var filter = {
-  urls: ["*://go/*"]
+const urlMap = {
+  google: 'https://google.com',
+  stan: 'https://stan.systems'
 };
 
+chrome.storage.sync.set({ urls: urlMap });
+
+const GO_REGEX = /http:\/\/go\/(.+)\//i;
+var filter = {
+  urls: ['*://go/*']
+};
+
+const getUrl = goLinkRequested =>
+  new Promise((resolve, reject) =>
+    chrome.storage.sync.get(['urls'], ({ urls }) =>
+      !!urls[goLinkRequested] ? resolve(urls[goLinkRequested]) : reject()
+    )
+  );
+
 const logOnBefore = async details => {
-  console.log(details);
-  const linkRequested = details.url.replace(GO_REGEX, "$1").toLowerCase();
-  fetch(`http://localhost:3000?url=${linkRequested}`)
-    .then(response => response.text())
-    .then(url =>
-      chrome.tabs.update({
-        url
-      })
-    );
+  const linkRequested = details.url.replace(GO_REGEX, '$1').toLowerCase();
+  getUrl(linkRequested).then(url =>
+    chrome.tabs.update({
+      url
+    })
+  );
   return { cancel: true };
 };
 
 chrome.webRequest.onBeforeRequest.addListener(logOnBefore, filter, [
-  "blocking"
+  'blocking'
 ]);
