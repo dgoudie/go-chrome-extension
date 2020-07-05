7b921829da859c8bfc4563c3954e03d4599423e1
dgoudie <34222208+dgoudie@users.noreply.github.com>
2020-07-05 13:11:27 -0500

    updated bump-manifest-version.js

--- utils/bump-manifest-version.js
+++ utils/bump-manifest-version.js
@@ -2,5 +2,6 @@ const fs = require('fs');
 
 const manifest = JSON.parse(fs.readFileSync("src/manifest.json"));
 
-console.log(manifest, process.env.npm_package_version);
-// fs.writeFileSync("file.json", JSON.stringify(data, null, 4));
+manifest.version = process.env.npm_package_version.replace(/^(\d+)\.?(\d+)\.?(\*|\d+)$/, '$1.$2')
+
+fs.writeFileSync("src/manifest.json", JSON.stringify(manifest, null, 4));
