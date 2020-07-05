2f412a085fbcd1a73edb958bf081def94f006003
dgoudie <34222208+dgoudie@users.noreply.github.com>
2020-07-05 13:18:05 -0500

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,81 +1,83 @@
-const isDevelopment = process.env.WEBPACK_MODE !== 'production';
-
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const ZipPlugin = require('zip-webpack-plugin')
 
-let plugins = [
-    new MiniCssExtractPlugin({
-        filename: isDevelopment ? '[name].css' : '[name].[hash].css',
-        chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
-    }),
-    new CopyPlugin([{ from: '**/!(*.ts|*.tsx|*.scss)', context: './src/' }]),
-]
+module.exports = (env, argv) => {
 
-!isDevelopment && (plugins = [...plugins, new ZipPlugin({filename: `${process.env.npm_package_name}-${process.env.npm_package_version}.zip`, path: '../'})])
+    const isDevelopment = argv.mode === 'development';
 
-module.exports = {
-    devtool: 'inline-source-map',
-    entry: {
-        'popup/index': './src/popup/index.tsx',
-        'background': './src/background.ts'
-    },
-    output: {
-        filename: '[name].js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader'
-            },
-            {
-                test: /\.module\.s(a|c)ss$/,
-                loader: [
-                    isDevelopment
-                        ? 'style-loader'
-                        : MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            modules: true,
-                            sourceMap: isDevelopment
+    let plugins = [
+        new MiniCssExtractPlugin({
+            filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+        }),
+        new CopyPlugin([{ from: '**/!(*.ts|*.tsx|*.scss)', context: './src/' }]),
+    ]
+    
+    !isDevelopment && (plugins = [...plugins, new ZipPlugin({filename: `${process.env.npm_package_name}-${process.env.npm_package_version}.zip`, path: '../'})])
+    return {
+        devtool: 'inline-source-map',
+        entry: {
+            'popup/index': './src/popup/index.tsx',
+            'background': './src/background.ts'
+        },
+        output: {
+            filename: '[name].js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.tsx?$/,
+                    loader: 'ts-loader'
+                },
+                {
+                    test: /\.module\.s(a|c)ss$/,
+                    loader: [
+                        isDevelopment
+                            ? 'style-loader'
+                            : MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                modules: true,
+                                sourceMap: isDevelopment
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment
+                            }
                         }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: isDevelopment
+                    ]
+                },
+                {
+                    test: /\.s(a|c)ss$/,
+                    exclude: /\.module.(s(a|c)ss)$/,
+                    loader: [
+                        isDevelopment
+                            ? 'style-loader'
+                            : MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: isDevelopment
+                            }
                         }
-                    }
-                ]
-            },
-            {
-                test: /\.s(a|c)ss$/,
-                exclude: /\.module.(s(a|c)ss)$/,
-                loader: [
-                    isDevelopment
-                        ? 'style-loader'
-                        : MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    {
-                        loader: 'sass-loader',
-                        options: {
-                            sourceMap: isDevelopment
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.scss']
-    },
-    plugins,
-    devServer: {
-        writeToDisk: true,
-        disableHostCheck: true
-    }
-};
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js', '.scss']
+        },
+        plugins,
+        devServer: {
+            writeToDisk: true,
+            disableHostCheck: true
+        }
+    };
+}
